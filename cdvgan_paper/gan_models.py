3f0e62caa5ece049774c587f235132d6eafcfaf6
Jossian <A01746745@itesm.mx>
2025-05-20 14:21:37 -0600

    change in derivative shape

--- cdvgan_paper/gan_models.py
+++ cdvgan_paper/gan_models.py
@@ -229,7 +229,6 @@ class cDVGAN(keras.Model):
                 # fake_signals = tf.cast(fake_signals, dtype=tf.float32)
                 
                 fake_derivatives = calculate_derivative(tf.cast(np.array(range(256)), tf.float32), fake_signals)
-                print(tf.shape(fake_derivatives).numpy())
                 # fake_derivatives = tf.cast(fake_derivatives, dtype=tf.float32)
      
                 # Get the logits for the fake signals
