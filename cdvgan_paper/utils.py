a30b62d0fb6ccb412ba89e8a42d8cc31da27857e
Jossian <A01746745@itesm.mx>
2025-05-27 10:19:12 -0600

    cambio en plot generations

--- cdvgan_paper/utils.py
+++ cdvgan_paper/utils.py
@@ -90,6 +90,8 @@ def plot_examples(data, classes, path):
         ax.plot(data[i])
         #print("classes: ",classes[i])
         #ax.set_title(str(classes[i].round(3)))
+        ax.set_ylim(-13, 7)  # Fijar el mismo rango Y para todos los subplots
+
         ax.set_title(", ".join(f"{x:.3f}" for x in classes[i]))
         #ax.set_title(classes[i].round(3))
         plt.subplots_adjust(hspace=0.4)
