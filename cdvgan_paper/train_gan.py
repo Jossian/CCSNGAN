410793a29ec51c55da15e06d514a57b0c285760f
Jossian <A01746745@itesm.mx>
2025-05-30 21:14:49 -0600

    modificacion consistency test

--- cdvgan_paper/train_gan.py
+++ cdvgan_paper/train_gan.py
@@ -56,11 +56,6 @@ unique,counts=np.unique(class_array_orig, return_counts=True)
 print("Classes, counts  originales del dataset: ")
 print(np.asarray((unique, counts)).T)
 
-class_array+=1
-mask = class_array != 0
-data = data[mask]
-class_array = class_array[mask]
-
 
 
 
@@ -69,10 +64,6 @@ metrics=compare_signal_datasets(data, data_orig)
 
 
 class_array = class_array.astype(np.int32)
-
-unique,counts=np.unique(class_array, return_counts=True)
-print("Classes, counts: ")
-print(np.asarray((unique, counts)).T)
 
 
 
