232e4227b8d6745327fb1ac6e48ad6a707295965
Jossian <A01746745@itesm.mx>
2025-06-03 18:45:18 -0600

    cambio de batch size a 32

--- cdvgan_paper/train_gan.py
+++ cdvgan_paper/train_gan.py
@@ -112,7 +112,7 @@ print("deriv_signal_length: ",deriv_signal_length)
 gan = choose_gan(gan_choice, signal_length, deriv_signal_length, deriv2_signal_length, num_classes, noise_dim)
 
 # Set batch size and number of epochs for training.
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 epochs = 500
 
 # Change to False if you don't want the GAN monitor to plot generated signals after each epoch.
