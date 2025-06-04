8bc7e4a03b1a90f6000a73b8ee46eca31498e643
Jossian <A01746745@itesm.mx>
2025-06-03 18:51:01 -0600

    cambio numero de epocas

--- cdvgan_paper/train_gan.py
+++ cdvgan_paper/train_gan.py
@@ -113,7 +113,7 @@ gan = choose_gan(gan_choice, signal_length, deriv_signal_length, deriv2_signal_l
 
 # Set batch size and number of epochs for training.
 BATCH_SIZE = 32
-epochs = 500
+epochs = 5
 
 # Change to False if you don't want the GAN monitor to plot generated signals after each epoch.
 callback = True
