80537197fe17388aeb95917289a1f5ac9d1d50b0
Jossian <A01746745@itesm.mx>
2025-07-01 11:32:05 -0600

    cambio plot hist

--- cdvgan_paper/train_gan.py
+++ cdvgan_paper/train_gan.py
@@ -115,7 +115,7 @@ gan = choose_gan(gan_choice, signal_length, deriv_signal_length, deriv2_signal_l
 
 # Set batch size and number of epochs for training.
 BATCH_SIZE = 16
-epochs = 5
+epochs = 500
 
 # Change to False if you don't want the GAN monitor to plot generated signals after each epoch.
 callback = True
